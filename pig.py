5ffb5494de60f25d23c7bd57ea67a60ef990dc3e
DeeMeyers <justdmeyers@gmail.com>
2020-02-19 16:29:56 -0500

    started

---
+++ pig.py
@@ -0,0 +1,23 @@
+import random
+
+dieNumbers = [1,2,3,4,5,6]
+
+
+class Game:
+    __init__(self)
+    pass
+
+
+class Die:
+    __init__(self, dieNumbers)
+    self.dieNumbers = dieNumbers
+
+    def roll(dieNumbers)
+        return #random pick from dieNumber list
+    pass
+
+class Player:
+    pass
+
+class Opponent:
+    pass
