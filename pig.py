c3061661840a629d9d2ab4f50c6e6727f8a7f436
DeeMeyers <justdmeyers@gmail.com>
2020-02-19 20:19:55 -0500

    i maybe know what im doing but im unclear how i go about testing

--- pig.py
+++ pig.py
@@ -1,23 +1,46 @@
-import random
+from random import randint
 
 dieNumbers = [1,2,3,4,5,6]
-
+maxScore = 100
+startScore = 0
 
 class Game:
-    __init__(self)
-    pass
-
+    def __init__(self):
+        self.opponent = Opponent()
+        self.player = Player("Betsy")
+        self.die = Die()
 
 class Die:
-    __init__(self, dieNumbers)
-    self.dieNumbers = dieNumbers
+    def __init__(self):
+        self.roll = randint(1, len(dieNumbers))
 
-    def roll(dieNumbers)
-        return #random pick from dieNumber list
-    pass
+    def __str__(self):
+        return f"A {self.roll} was rolled"
+
 
 class Player:
-    pass
+    def __init__(self, name):
+        self.roundScore = 0
+        self.totalScore = 0
+        self.name = name
+        self.opponent = Opponent()
+
+    def playerTurn(self):
+        curentRoll = Die()
+        print(f"{self.name}! You rolled a {currentRoll).")
+        if currentRoll == 1:
+            print(f"Bad luck {self.name}, YOU GET NOTHING!")
+            Opponent()
+        
+
+
 
 class Opponent:
-    pass
+    def __init__(self):
+        self.roundScore = 0
+        self.totalScore = 0
+        self.name = 'STORMAGEDEON'
+        
+
+
+    
